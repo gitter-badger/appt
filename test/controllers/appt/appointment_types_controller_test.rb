da8350b66aafc293d63bb3b87cdb7bbca2b8f831
Paul Tyng <paul@paultyng.net>
2015-08-04 21:56:17 -0400

    Add appt types controller placeholder

---
+++ test/controllers/appt/appointment_types_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+module Appt
+  class AppointmentTypesControllerTest < ActionController::TestCase
+    setup do
+      @routes = Engine.routes
+    end
+
+    # test "the truth" do
+    #   assert true
+    # end
+  end
+end
+
