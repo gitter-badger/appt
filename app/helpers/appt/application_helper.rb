ff64704dfb09aabdce587ea6a010fe9b07273d41
Paul Tyng <paul@paultyng.net>
2015-08-04 21:53:55 -0400

    Add breadcrumbs helpers

--- app/helpers/appt/application_helper.rb
+++ app/helpers/appt/application_helper.rb
@@ -8,6 +8,24 @@ module Appt
       end
     end
 
+    def breadcrumbs(&block)
+      content_for(:breadcrumbs) do
+        capture do
+          content_tag(:ol, class: 'breadcrumb') do
+            concat breadcrumb 'Home', root_path
+            concat capture(&block) if block_given?
+            concat content_tag(:li, title, class: 'active')
+          end
+        end
+      end
+    end
+
+    def breadcrumb(name, options, html_options = nil, &block)
+      content_tag(:li) do
+        concat link_to name, options, html_options, &block
+      end
+    end
+
     def time_ago_tag(value)
       if value.nil?
         nil
