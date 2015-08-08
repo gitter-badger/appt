77b70abe6eab533ad12b5ee1c9eb43aa3525662e
Paul Tyng <paul@paultyng.net>
2015-08-08 18:50:22 -0400

    Remove unnecessary patch

--- config/initializers/workhours_patch.rb
+++
@@ -1,8 +0,0 @@
-Workhours::Period.class_eval do
-  def initialize_with_exclude_end(wday, beginning, ending)
-    initialize_without_exclude_end(wday, beginning, ending)
-
-    @shift = Tod::Shift.new(@shift.beginning, @shift.ending, true)
-  end
-  alias_method_chain :initialize, :exclude_end
-end
