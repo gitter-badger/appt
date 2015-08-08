4ffa263dfe7d14995e0afb5d94698f15de952a4b
Paul Tyng <paul@paultyng.net>
2015-08-08 17:44:58 -0400

    Minor syntax fixes

--- app/helpers/appt/calendar_event_helpers.rb
+++ app/helpers/appt/calendar_event_helpers.rb
@@ -1,9 +1,9 @@
 module Appt
   module CalendarEventHelpers
-    def calendar_event_calendar(timezone, calendar_events, options={}, &block)
+    def calendar_event_calendar(timezone, calendar_events, options = {}, &block)
       calendar = BootstrapMonthCalendar.new(self, options.merge(timezone: timezone, events: calendar_events))
 
-      calendar_render = Proc.new do |date, events|
+      calendar_render = proc do |date, events|
         concat content_tag :h3, date.day
         concat capture(date, events, &block)
       end
