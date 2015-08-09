944efd72c9f6bca47b96b0d223c22a0b0dfb65ff
Paul Tyng <paul@paultyng.net>
2015-08-09 17:45:45 -0400

    Make events optional for calendar display

--- app/helpers/appt/calendar_event_helpers.rb
+++ app/helpers/appt/calendar_event_helpers.rb
@@ -1,7 +1,12 @@
 module Appt
   module CalendarEventHelpers
-    def calendar_event_calendar(timezone, calendar_events, options = {}, &block)
-      calendar = BootstrapMonthCalendar.new(self, options.merge(timezone: timezone, events: calendar_events))
+    def calendar_event_calendar(timezone, calendar_events = nil, options = {}, &block)
+      calendar_events, options = nil, calendar_events if calendar_events.is_a?(Hash)
+
+      options.merge!(timezone: timezone)
+      options.merge!(events: calendar_events) if calendar_events
+
+      calendar = BootstrapMonthCalendar.new(self, options)
 
       calendar_render = proc do |date, events|
         concat capture(date, events, &block)
