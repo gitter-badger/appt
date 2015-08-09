9b7e0c7bbc1017ddab5223189c81feec6be52be2
Paul Tyng <paul@paultyng.net>
2015-08-09 11:58:00 -0400

    Add today link to calendar view

--- lib/bootstrap_month_calendar.rb
+++ lib/bootstrap_month_calendar.rb
@@ -2,7 +2,7 @@ class BootstrapMonthCalendar < SimpleCalendar::MonthCalendar
   def initialize_with_bootstrap(view_context, opts = {})
     opts.reverse_merge!(
       table: { class: 'table calendar' },
-      header: { class: 'pagination' },
+      header: { class: 'pager' },
       previous_link: bootstrap_previous_link,
       next_link: bootstrap_next_link,
     )
@@ -18,21 +18,29 @@ class BootstrapMonthCalendar < SimpleCalendar::MonthCalendar
 
   def render_header_with_bootstrap
     capture do
-      content_tag :ul, { class: 'pagination' }.merge(get_option(:header)) do
-        concat get_option(:previous_link, param_name, date_range)
-        concat get_option(:title, start_date)
-        concat get_option(:next_link, param_name, date_range)
+      content_tag :nav do
+        content_tag :ul, get_option(:header) do
+          concat get_option(:previous_link, param_name, date_range)
+          concat get_option(:title, start_date)
+          concat content_tag :li, link_to('Today', param_name => nil) unless current_month?
+          concat get_option(:next_link, param_name, date_range)
+        end
       end
-    end
+    end +
+      link_to('Today', { params => nil }, class: 'btn btn-link')
   end
   alias_method_chain :render_header, :bootstrap
 
   def bootstrap_previous_link
-    ->(param, date_range){ content_tag :li, link_to(raw('&laquo;'), param => date_range.first - 1.day) }
+    ->(p, r){ content_tag :li, link_to(raw('&laquo;'), p => r.first - 1.day), class: 'previous' }
   end
 
   def bootstrap_next_link
-    ->(param, date_range){ content_tag :li, link_to(raw('&raquo;'), param => date_range.last + 1.day) }
+    ->(p, r){ content_tag :li, link_to(raw('&raquo;'), p => r.last + 1.day), class: 'next' }
+  end
+
+  def current_month?
+    timezone.now.to_date.beginning_of_month == start_date.beginning_of_month
   end
 end
 
