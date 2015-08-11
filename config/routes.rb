dbcd4fe9def0295fb79926f914e741fa69319e5b
Paul Tyng <paul@paultyng.net>
2015-08-10 22:16:02 -0400

    Add placeholder route for customer by email

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Appt::Engine.routes.draw do
   end
   resources :external_calendars, path: 'external-calendars'
   resources :appointment_types, path: 'appointment-types'
+  get 'customers/:email', to: 'customers#show', as: :customer
   root to: 'home#index'
 end
 
